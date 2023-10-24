a8dd4f1e5cc591fa637289ad4fa75deabe8313c0
dong <dpwk8@naver.com>
2023-10-24 17:49:03 +0900

    refactor: componentScan 범위 변경
    >> `본문 들어갈자리`

--- backend/member/src/main/java/com/bookdone/member/MemberApplication.java
+++ backend/member/src/main/java/com/bookdone/member/MemberApplication.java
@@ -4,14 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@ComponentScan(basePackages = {"com.bookdone"})
 public class MemberApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MemberApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApplication.class, args);
+    }
 
 }

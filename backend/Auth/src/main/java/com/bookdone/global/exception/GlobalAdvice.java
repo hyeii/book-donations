2bdeff9bee559d09f545424221c8ff4d6539b4f7
dong <dpwk8@naver.com>
2023-10-25 15:05:57 +0900

    fix: objectmapper exception 수정

--- backend/Auth/src/main/java/com/bookdone/global/exception/GlobalAdvice.java
+++ backend/Auth/src/main/java/com/bookdone/global/exception/GlobalAdvice.java
@@ -2,6 +2,7 @@ package com.bookdone.global.exception;
 
 import com.bookdone.global.response.BaseResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,20 +10,22 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import javax.servlet.http.HttpServletResponse;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalAdvice {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<?> illegalArgumentResponse(IllegalArgumentException ex) {
-        return new BaseResponse().fail(ex.getMessage(), HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        return BaseResponse.fail(ex.getMessage(), HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(JsonProcessingException.class)
     public ResponseEntity<?> jsonProcessingResponse(JsonProcessingException ex) {
-        return new BaseResponse().fail(ex.getMessage(), HttpServletResponse.SC_BAD_REQUEST);
+
+        return BaseResponse.fail(ex.getMessage(), HttpServletResponse.SC_BAD_REQUEST);
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<?> runtimeResponse(RuntimeException ex) {
-        return new BaseResponse().fail(ex.getMessage(), HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        return BaseResponse.fail(ex.getMessage(), HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
     }
 }
--- backend/Auth/src/main/java/com/bookdone/global/util/FeignResponse.java
+++ backend/Auth/src/main/java/com/bookdone/global/util/FeignResponse.java
@@ -4,17 +4,22 @@ import com.bookdone.global.response.FailResponse;
 import com.bookdone.global.response.SuccessResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 public class FeignResponse {
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
     public static <T> T extractDataFromResponse(ResponseEntity<?> responseEntity, Class<T> clazz) throws RuntimeException, JsonProcessingException {
         HttpStatus status = responseEntity.getStatusCode();
-        Map<String, Object> objectMap = (Map<String, Object>) responseEntity.getBody();
+        log.info("body : {}", responseEntity.getBody());
+        Map<String, Object> objectMap = (HashMap<String, Object>) responseEntity.getBody();
+        log.info("map : {} ", objectMap);
         boolean success = (boolean) objectMap.get("success");
 
         // 응답 코드가 2xx가 아니면 오류 던지기

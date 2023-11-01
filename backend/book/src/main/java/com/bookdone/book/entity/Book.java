99a8efc9e947d7d91dfb5058c7200f06f291f9f0
차건영 <chakunyou@naver.com>
2023-11-01 17:53:44 +0900

    refactor: book entity text로 변경

--- backend/book/src/main/java/com/bookdone/book/entity/Book.java
+++ backend/book/src/main/java/com/bookdone/book/entity/Book.java
@@ -21,55 +21,55 @@ public class Book {
 	@Column(name = "ID")
 	private Long id;
 
-	@Column(name = "TITLE", length = 5000)
+	@Column(name = "TITLE", columnDefinition = "TEXT")
 	private String title;
 
-	@Column(name = "VOL", length = 1000)
+	@Column(name = "VOL", columnDefinition = "TEXT")
 	private String vol;
 
-	@Column(name = "SERIES_TITLE", length = 5000)
+	@Column(name = "SERIES_TITLE", columnDefinition = "TEXT")
 	private String seriesTitle;
 
-	@Column(name = "SERIES_NO", length = 1000)
+	@Column(name = "SERIES_NO", columnDefinition = "TEXT")
 	private String seriesNo;
 
-	@Column(name = "AUTHOR", length = 5000)
+	@Column(name = "AUTHOR", columnDefinition = "TEXT")
 	private String author;
 
-	@Column(name = "EA_ISBN", length = 1000)
+	@Column(name = "EA_ISBN", columnDefinition = "TEXT")
 	private String isbn;
 
-	@Column(name = "EA_ADD_CODE", length = 1000)
+	@Column(name = "EA_ADD_CODE", columnDefinition = "TEXT")
 	private String eaAddCode;
 
-	@Column(name = "SET_ISBN", length = 1000)
+	@Column(name = "SET_ISBN", columnDefinition = "TEXT")
 	private String setIsbn;
 
-	@Column(name = "SET_ADD_CODE", length = 1000)
+	@Column(name = "SET_ADD_CODE", columnDefinition = "TEXT")
 	private String setAddCode;
 
-	@Column(name = "SUBJECT", length = 5000)
+	@Column(name = "SUBJECT", columnDefinition = "TEXT")
 	private String subject;
 
-	@Column(name = "TITLE_URL", length = 5000)
+	@Column(name = "TITLE_URL", columnDefinition = "TEXT")
 	private String titleUrl;
 
-	@Column(name = "PUBLISHER_URL", length = 5000)
+	@Column(name = "PUBLISHER_URL", columnDefinition = "TEXT")
 	private String publisherUrl;
 
-	@Column(name = "INPUT_DATE", length = 500)
+	@Column(name = "INPUT_DATE", columnDefinition = "TEXT")
 	private String inputDate;
 
-	@Column(name = "UPDATE_DATE", length = 500)
+	@Column(name = "UPDATE_DATE", columnDefinition = "TEXT")
 	private String updateDate;
 
-	@Column(name = "BOOK_SIZE", length = 1000)
+	@Column(name = "BOOK_SIZE", columnDefinition = "TEXT")
 	private String bookSize;
 
-	@Column(name = "PAGE", length = 1000)
+	@Column(name = "PAGE", columnDefinition = "TEXT")
 	private String page;
 
-	@Column(name = "PUBLISHER", length = 5000)
+	@Column(name = "PUBLISHER", columnDefinition = "TEXT")
 	private String publisher;
 
 	public BookDto toDto() {

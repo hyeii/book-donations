2d2295a1308ca3e2dd5d0163fc61a965d0786794
김용균 <kyg4199@naver.com>
2023-11-06 16:34:59 +0900

    fix: memberfeign 재수정

--- backend/donation/src/main/java/com/bookdone/client/api/MemberClient.java
+++ backend/donation/src/main/java/com/bookdone/client/api/MemberClient.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
@@ -13,6 +14,6 @@ public interface MemberClient {
     @GetMapping("/api/members/nicknames/{memberId}")
     ResponseEntity<?> getNickname(@PathVariable Long memberId);
 
-    @GetMapping("/api/members/nicknames")
+    @PostMapping("/api/members/nicknames")
     ResponseEntity<?> getNicknameList(@RequestBody List<Long> memberIdList);
 }

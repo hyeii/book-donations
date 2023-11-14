87dc6f75255f2698a796fa386c6de00447770122
차건영 <chakunyou@naver.com>
2023-11-14 17:44:52 +0900

    feat: merge

--- frontend/bookdone/lib/chat/page/chat_main.dart
+++ frontend/bookdone/lib/chat/page/chat_main.dart
@@ -2,15 +2,8 @@ import 'package:bookdone/chat/service/rest_chat.dart';
 import 'package:bookdone/chat/widgets/chatroom_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:stomp_dart_client/stomp.dart';
-import 'package:stomp_dart_client/stomp_config.dart';
-import 'package:stomp_dart_client/stomp_frame.dart';
-import '../../top/page/top_navigation_bar.dart';
 import '../model/chat.dart';
-import '../service/stomp_service.dart';
 
 class ChatMain extends HookConsumerWidget {
   const ChatMain({super.key});

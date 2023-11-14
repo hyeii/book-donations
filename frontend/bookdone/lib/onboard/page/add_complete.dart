c0c309e1723182cfd013599eb8f78196948802fd
윤혜민 <hmyoon97@naver.com>
2023-11-14 13:49:30 +0900

    fix: 회원가입 시 경로 수정 및 오류 수정

--- frontend/bookdone/lib/onboard/page/add_complete.dart
+++ frontend/bookdone/lib/onboard/page/add_complete.dart
@@ -1,43 +1,62 @@
+import 'package:bookdone/onboard/service/set_user_api.dart';
+import 'package:bookdone/regist/page/regist_data.dart';
 import 'package:bookdone/router/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class AddComplete extends HookWidget {
+class AddComplete extends HookConsumerWidget {
   const AddComplete({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: Color(0xff928C85),
       body: Padding(
         padding: EdgeInsets.symmetric(
             horizontal: MediaQuery.of(context).size.width / 10),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             SizedBox(
               height: 20,
             ),
             Text(
-              '반가워용',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w800),
+              '반갑습니다!',
+              style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w800,
+                  color: Colors.white),
             ),
             Text(
               '책도네에서 선순환을 시작해 보세요.',
-              style: TextStyle(
-                fontSize: 18,
-              ),
+              style: TextStyle(fontSize: 15, color: Colors.white),
             ),
-            ElevatedButton(
-              onPressed: () {
-                // context.go('/home');
-                // HomeRoute().go(context);
-                TopPageRoute().go(context);
+            SizedBox(
+              height: 20,
+            ),
+            GestureDetector(
+              onTap: () {
+                SetUserApi.updateMyInfo(ref);
+                ref.invalidate(registerRegionStateProvider);
+                ref.invalidate(registerRegionCodeStateProvider);
+                HomeRoute().go(context);
               },
               // shape: CircleBorder(),
-              child: Text('ㄱㄱ'),
-            )
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 203, 197, 190),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                padding: const EdgeInsets.all(25),
+                child: Icon(Icons.arrow_forward, color: Colors.white),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
           ],
         ),
       ),

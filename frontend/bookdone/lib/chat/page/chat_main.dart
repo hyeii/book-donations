901dc12c1cf2ffa0be530bad0e1a1480e223e34f
차건영 <chakunyou@naver.com>
2023-11-15 10:18:37 +0900

    feat: 채팅 없을 때 오류 해결, 디자인 변경

--- frontend/bookdone/lib/chat/page/chat_main.dart
+++ frontend/bookdone/lib/chat/page/chat_main.dart
@@ -12,37 +12,71 @@ class ChatMain extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final chatRooms = useState<List<ChatRoomResponse>>([]);
     final restClient = ref.read(restApiClientProvider);
+    final isChatRoomFetched = useState(false);
+
+    fetchChatRooms() async {
+      try {
+        final response = await restClient.chatRoomList();
+        chatRooms.value = response.data;
+        isChatRoomFetched.value = true;
+      } catch (e) {
+        print('Error fetching chat rooms: $e');
+      }
+    }
 
     useEffect(() {
-      fetchChatRooms() async {
-        try {
-          final response = await restClient.chatRoomList();
-          chatRooms.value = response.data;
-          print(chatRooms.value);
-        } catch (e) {
-          print('Error fetching chat rooms: $e');
-        }
+      if (chatRooms.value.isEmpty && !isChatRoomFetched.value) {
+        fetchChatRooms();
       }
 
-      fetchChatRooms();
       return null;
     }, []);
 
     return Scaffold(
       appBar: AppBar(
         centerTitle: false,
-        title: Text("채팅"),
+        title: const Text("채팅"), // Using const for text widget
       ),
-      body: chatRooms.value.isEmpty
-          ? Center(child: CircularProgressIndicator())
-          : ListView.separated(
-              itemCount: chatRooms.value.length,
-              itemBuilder: (context, index) {
-                final chatRoom = chatRooms.value[index];
-                return ChatRoomCard(chatRoom: chatRoom); // 수정된 ChatRoomCard 사용
-              },
-              separatorBuilder: (context, index) => SizedBox.shrink(),
+      body: chatRooms.value.isEmpty && isChatRoomFetched.value
+          ? Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              Icons.chat_bubble_outline,
+              size: 100,
+              color: Colors.grey[400],
             ),
+            Text(
+              "채팅이 없습니다.",
+              style: TextStyle(
+                color: Colors.grey[600],
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Text(
+                "채팅을 시작해보세요.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.grey[600],
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ],
+        ),
+      )
+          : ListView.separated(
+        itemCount: chatRooms.value.length,
+        itemBuilder: (context, index) {
+          final chatRoom = chatRooms.value[index];
+          return ChatRoomCard(chatRoom: chatRoom);
+        },
+        separatorBuilder: (context, index) => const SizedBox.shrink(),
+      ),
     );
   }
 }

332828d1d45446b5fb9468f27f80c6b12d16dd24
윤혜민 <hmyoon97@naver.com>
2023-11-16 16:04:25 +0900

    feat: 기부글 이미지 확대

--- frontend/bookdone/lib/article/page/article_main.dart
+++ frontend/bookdone/lib/article/page/article_main.dart
@@ -33,6 +33,9 @@ class ArticleMain extends HookConsumerWidget {
 
     final tradeIdFromServer = useState<int>(0);
 
+    var openImage = useState(false);
+    var curImage = useState('');
+
     Future<ArticleData> getArticleInfo() async {
       ArticleRespByid data = await restClient.getArticleById(id);
       ArticleData articleInfo = data.data;
@@ -118,135 +121,175 @@ class ArticleMain extends HookConsumerWidget {
               horizontal: MediaQuery.of(context).size.width / 10),
           child: SingleChildScrollView(
             child: Center(
-              child: Column(
+              child: Stack(
                 children: [
-                  CachedNetworkImage(
-                    width: 200,
-                    imageUrl:
-                        bookData.value != null && bookData.value!.titleUrl != ''
+                  Column(
+                    children: [
+                      CachedNetworkImage(
+                        width: 200,
+                        imageUrl: bookData.value != null &&
+                                bookData.value!.titleUrl != ''
                             ? bookData.value!.titleUrl
                             : 'assets/images/sample-bookdone.png',
-                    placeholder: (context, url) => CircularProgressIndicator(),
-                    errorWidget: (context, url, error) => Image(
-                        image: AssetImage('assets/images/sample-bookdone.png')),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Text(
-                    bookData.value != null ? bookData.value!.title : '',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 15.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Divider(thickness: 1, height: 1),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        if (articleData.value != null)
-                          articleData.value!.historyResponseList.isNotEmpty
-                              ? GestureDetector(
-                                  onTap: () {
-                                    print(id);
-                                    HistoryRoute(
-                                      donationId: id,
-                                      title: bookData.value != null
-                                          ? bookData.value!.title
-                                          : '',
-                                      titleUrl: bookData.value != null
-                                          ? bookData.value!.titleUrl
-                                          : '',
-                                    ).push(context);
-                                  },
-                                  child: Text(
-                                    '${articleData.value!.historyResponseList.length}개의 히스토리',
-                                  ),
-                                )
-                              : Text('히스토리가 없습니다'),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Divider(thickness: 1, height: 1),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Text(
-                          "기부자의 글",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        if (articleData.value != null)
-                          Text(
-                            articleData.value!.content,
-                          ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        articleData.value != null
-                            ? Container(
-                                // margin: EdgeInsets.all(10),
-                                child: GridView.builder(
-                                  padding: EdgeInsets.all(0),
-                                  shrinkWrap: true,
-                                  itemCount:
-                                      articleData.value!.imageUrlList.length,
-                                  gridDelegate:
-                                      SliverGridDelegateWithFixedCrossAxisCount(
-                                    crossAxisCount: 3, //1 개의 행에 보여줄 사진 개수
-                                    childAspectRatio: 1 / 1, //사진 의 가로 세로의 비율
-                                    mainAxisSpacing: 10, //수평 Padding
-                                    crossAxisSpacing: 10, //수직 Padding
-                                  ),
-                                  itemBuilder:
-                                      (BuildContext context, int index) {
-                                    // print(
-                                    //     '${articleData.value!.imageUrlList[index]}');
-                                    return Container(
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(5),
-                                        image: DecorationImage(
-                                          fit: BoxFit
-                                              .cover, //사진을 크기를 상자 크기에 맞게 조절
-                                          image: NetworkImage(articleData
-                                              .value!.imageUrlList[index]),
-                                        ),
+                        placeholder: (context, url) =>
+                            CircularProgressIndicator(),
+                        errorWidget: (context, url, error) => Image(
+                            image: AssetImage(
+                                'assets/images/sample-bookdone.png')),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        bookData.value != null ? bookData.value!.title : '',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 15.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Divider(thickness: 1, height: 1),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            if (articleData.value != null)
+                              articleData.value!.historyResponseList.isNotEmpty
+                                  ? GestureDetector(
+                                      onTap: () {
+                                        print(id);
+                                        HistoryRoute(
+                                          donationId: id,
+                                          title: bookData.value != null
+                                              ? bookData.value!.title
+                                              : '',
+                                          titleUrl: bookData.value != null
+                                              ? bookData.value!.titleUrl
+                                              : '',
+                                        ).push(context);
+                                      },
+                                      child: Text(
+                                        '${articleData.value!.historyResponseList.length}개의 히스토리',
                                       ),
-                                    );
-                                  },
-                                ),
-                              )
-                            : Text(' '),
-                        SizedBox(
-                          height: 20,
+                                    )
+                                  : Text('히스토리가 없습니다'),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Divider(thickness: 1, height: 1),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              "기부자의 글",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            if (articleData.value != null)
+                              Text(
+                                articleData.value!.content,
+                              ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            articleData.value != null
+                                ? Container(
+                                    // margin: EdgeInsets.all(10),
+                                    child: GridView.builder(
+                                      padding: EdgeInsets.all(0),
+                                      shrinkWrap: true,
+                                      itemCount: articleData
+                                          .value!.imageUrlList.length,
+                                      gridDelegate:
+                                          SliverGridDelegateWithFixedCrossAxisCount(
+                                        crossAxisCount: 3, //1 개의 행에 보여줄 사진 개수
+                                        childAspectRatio:
+                                            1 / 1, //사진 의 가로 세로의 비율
+                                        mainAxisSpacing: 10, //수평 Padding
+                                        crossAxisSpacing: 10, //수직 Padding
+                                      ),
+                                      itemBuilder:
+                                          (BuildContext context, int index) {
+                                        // print(
+                                        //     '${articleData.value!.imageUrlList[index]}');
+                                        return GestureDetector(
+                                          onTap: () {
+                                            print('잉이ㅐㅇ');
+                                            openImage.value = true;
+                                            curImage.value = articleData
+                                                .value!.imageUrlList[index];
+                                          },
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(5),
+                                              image: DecorationImage(
+                                                fit: BoxFit
+                                                    .cover, //사진을 크기를 상자 크기에 맞게 조절
+                                                image: NetworkImage(articleData
+                                                    .value!
+                                                    .imageUrlList[index]),
+                                              ),
+                                            ),
+                                          ),
+                                        );
+                                      },
+                                    ),
+                                  )
+                                : Text(' '),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Divider(thickness: 1, height: 1),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              "책 정보",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            if (bookData.value != null)
+                              Text(
+                                bookData.value!.author,
+                              ),
+                            Text(
+                              bookData.value != null
+                                  ? bookData.value!.publisher
+                                  : '',
+                            ),
+                            Text(
+                              bookData.value != null
+                                  ? bookData.value!.isbn
+                                  : '',
+                            ),
+                            SizedBox(
+                              height: 50,
+                            )
+                          ],
                         ),
-                        Divider(thickness: 1, height: 1),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Text(
-                          "책 정보",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        if (bookData.value != null)
-                          Text(
-                            bookData.value!.author,
-                          ),
-                        Text(
-                          bookData.value != null
-                              ? bookData.value!.publisher
-                              : '',
-                        ),
-                        Text(
-                          bookData.value != null ? bookData.value!.isbn : '',
-                        ),
-                        SizedBox(
-                          height: 50,
-                        )
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
+                  if (openImage.value)
+                    Positioned(
+                      right: 0,
+                      left: 0,
+                      // left: MediaQuery.of(context).size.width,
+                      child: GestureDetector(
+                        onTap: () {
+                          openImage.value = false;
+                          curImage.value = '';
+                        },
+                        child: CachedNetworkImage(
+                          width: MediaQuery.of(context).size.width * 3 / 4,
+                          imageUrl: curImage.value,
+                          placeholder: (context, url) =>
+                              CircularProgressIndicator(),
+                          errorWidget: (context, url, error) => Image(
+                              image: AssetImage(
+                                  'assets/images/sample-bookdone.png')),
+                        ),
+                      ),
+                    ),
                 ],
               ),
             ),

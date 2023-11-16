a48dfc5fafd1618606b0cc3e1562d73abcb0b5ee
차건영 <chakunyou@naver.com>
2023-11-16 11:15:12 +0900

    feat: 버튼 하나로 변경, 취소 추가

--- frontend/bookdone/lib/chat/widgets/trade_button.dart
+++ frontend/bookdone/lib/chat/widgets/trade_button.dart
@@ -42,7 +42,6 @@ class TradeButton extends HookConsumerWidget {
         print('tradeId: $tradeId');
         print('receiveUserId: ${receiveUserId.value}');
         print('tradeStatus: ${tradeStatus.value}');
-
       } catch (e) {
         print('Error fetching trade widget call info: $e');
       }
@@ -52,37 +51,24 @@ class TradeButton extends HookConsumerWidget {
       fetchTradeInfo();
     }, []);
 
-    /// 왼쪽 버튼 활성화 조건
-    bool isLeftButtonEnabled() {
-      return !isLeftButtonPressed.value &&
-          userId.value != receiveUserId.value &&
-          (tradeStatus.value == "DONATION_REQUESTED" || tradeStatus.value == "DONATION_CONFIRMED");
-    }
-
-    /// 오른쪽 버튼 활성화 조건
-    bool isRightButtonEnabled() {
-      print(isRightButtonPressed.value);
-      print(tradeStatus.value);
-
-      return !isRightButtonPressed.value &&
-          userId.value == receiveUserId.value &&
-          (tradeStatus.value == "NONE" || tradeStatus.value == "COMPLETION_REQUESTED");
-    }
-
     Future<void> updateTradeStatus() async {
       try {
         switch (tradeStatus.value) {
           case "NONE":
-            await restClient.reservationRequestTrade(donationId.value, receiveUserId.value);
+            await restClient.reservationRequestTrade(
+                donationId.value, receiveUserId.value);
             break;
           case "DONATION_REQUESTED":
-            await restClient.reservationConfirmTrade(donationId.value, receiveUserId.value);
+            await restClient.reservationConfirmTrade(
+                donationId.value, receiveUserId.value);
             break;
           case "DONATION_CONFIRMED":
-            await restClient.completionRequestTrade(donationId.value, receiveUserId.value);
+            await restClient.completionRequestTrade(
+                donationId.value, receiveUserId.value);
             break;
           case "COMPLETION_REQUESTED":
-            await restClient.completionConfirmTrade(donationId.value, receiveUserId.value);
+            await restClient.completionConfirmTrade(
+                donationId.value, receiveUserId.value);
             break;
           default:
             print("Invalid trade status");
@@ -93,60 +79,50 @@ class TradeButton extends HookConsumerWidget {
       }
     }
 
+    bool isButtonEnabled() {
+      if (userId.value == receiveUserId.value) {
+        return tradeStatus.value == "NONE" || tradeStatus.value == "DONATION_CONFIRMED";
+      } else {
+        return tradeStatus.value == "DONATION_REQUESTED" || tradeStatus.value == "COMPLETION_REQUESTED";
+      }
+    }
 
-    /// 왼쪽 버튼 액션
-    leftButtonAction() {
-      if (isLeftButtonEnabled()) {
-        isLeftButtonPressed.value = true; // 버튼이 눌렸음을 표시
-        print("left 서버로 요청 보내기 dummy ------------------------------");
-        // TODO: 서버에 요청 보내는 로직 추가
+    String buttonText() {
+      // 현재 상태에 따라 버튼 텍스트 결정
+      switch (tradeStatus.value) {
+        case "NONE":
+        case "DONATION_REQUESTED":
+          return "기부 예약";
+        case "DONATION_CONFIRMED":
+        case "COMPLETION_REQUESTED":
+          return "완료 확인";
+        case "COMPLETION_CONFIRMED":
+          return "거래 완료";
+        default:
+          return "액션";
+      }
+    }
+
+    // 버튼 액션
+    buttonAction() {
+      // 현재 상태에 따라 적절한 네트워크 요청 수행
+      if (isButtonEnabled()) {
         updateTradeStatus();
       }
     }
 
-    /// 오른쪽 버튼 액션
-    rightButtonAction() {
-      if (isRightButtonEnabled()) {
-        isRightButtonPressed.value = true; // 버튼이 눌렸음을 표시
-        print("right 서버로 요청 보내기 dummy ------------------------------");
-        // TODO: 서버에 요청 보내는 로직 추가
-        updateTradeStatus(); // Call the update function
-      }
+    // 취소 버튼 액션 정의
+    cancelTradeAction() async {
+      // 거래 취소 로직 구현
+      print("거래 취소 요청");
+      // TODO: 서버에 거래 취소 요청 보내는 로직 추가
     }
 
-    /// 버튼 텍스트 변경
-    String leftButtonText() {
-      switch (tradeStatus.value) {
-        case "NONE":
-          return "기부 완료";
-        case "DONATION_REQUESTED":
-          return "완료 확인";
-        case "DONATION_CONFIRMED":
-          return "기부 완료";
-        case "COMPLETION_REQUESTED":
-          return "기부 완료";
-        case "COMPLETION_CONFIRMED":
-          return "거래 완료";
-        default:
-          return "";
-      }
-    }
-
-    String rightButtonText() {
-      switch (tradeStatus.value) {
-        case "NONE":
-          return "기부 예약";
-        case "DONATION_REQUESTED":
-          return "기부 예약";
-        case "DONATION_CONFIRMED":
-          return "완료 확인";
-        case "COMPLETION_REQUESTED":
-          return "예약 확인";
-        case "COMPLETION_CONFIRMED":
-          return "거래 완료";
-        default:
-          return "";
-      }
+    // 취소 버튼 활성화 조건
+    bool isCancelButtonEnabled() {
+      // 취소 버튼이 활성화되는 조건을 여기에 정의
+      return tradeStatus.value !=
+          "COMPLETION_CONFIRMED"; // 예시: 완료 확인된 상태가 아닐 때 활성화
     }
 
     return Container(
@@ -156,16 +132,16 @@ class TradeButton extends HookConsumerWidget {
           Expanded(
             flex: 3,
             child: ElevatedButton(
-              onPressed: isLeftButtonEnabled() ? leftButtonAction : null,
-              child: Text(leftButtonText()),
+              onPressed: isButtonEnabled() ? buttonAction : null,
+              child: Text(buttonText()),
             ),
           ),
           SizedBox(width: 10),
           Expanded(
             flex: 3,
             child: ElevatedButton(
-              onPressed: isRightButtonEnabled() ? rightButtonAction : null,
-              child: Text(rightButtonText()),
+              onPressed: isCancelButtonEnabled() ? cancelTradeAction : null,
+              child: Text("거래 취소"),
             ),
           ),
         ],
